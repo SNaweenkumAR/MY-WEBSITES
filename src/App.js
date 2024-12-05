b455ddd68339b681ed037276a91e0cb449b90ba9
SNaweenkumAR <naween1924@gmail.com>
2024-12-05 18:50:18 +0530

    add Mywork

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Component/Navbar/Navbar'
 import About from  './Component/About/About'
 import Hero from './Component/Hero/Hero'
 import Services from './Component/Services/Services'
+import MyWork  from './Component/MyWork/MyWork'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
   <Hero/>
   <About/>
   <Services/>
+  <MyWork/>
 </div>    
     
   );
