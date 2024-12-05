38f72c6537ddc9c3a848669581debc89e09cca6b
SNaweenkumAR <naween1924@gmail.com>
2024-12-05 18:48:04 +0530

    add Services

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Navbar from './Component/Navbar/Navbar'
 import About from  './Component/About/About'
 import Hero from './Component/Hero/Hero'
+import Services from './Component/Services/Services'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
   <Navbar/>
   <Hero/>
   <About/>
+  <Services/>
 </div>    
     
   );
