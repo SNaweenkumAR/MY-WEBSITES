95d8890a597a0175f69a6285f4f42a844e271cfe
SNaweenkumAR <naween1924@gmail.com>
2024-12-05 13:01:15 +0530

    app updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import Navbar from './Component/Navbar/Navbar'
+import About from  './Component/About/About'
 
 function App() {
   return (
 <div>
   <Navbar/>
+  <About/>
 </div>    
     
   );
