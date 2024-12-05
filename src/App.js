f3fc4a52ad585945fe7af738d12ab8fb4759b9a6
SNaweenkumAR <naween1924@gmail.com>
2024-12-05 13:03:31 +0530

    add hero

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import Navbar from './Component/Navbar/Navbar'
 import About from  './Component/About/About'
+import Hero from './Component/Hero/Hero'
 
 function App() {
   return (
 <div>
   <Navbar/>
+  <Hero/>
   <About/>
 </div>    
     
